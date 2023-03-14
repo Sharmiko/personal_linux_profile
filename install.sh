e39da5feee0738cc7cdb2ef4b3f142a7158e1010
Sharmiko <gsharmiashvili99@gmail.com>
2023-03-14 23:23:37 +0400

    fix tag

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install window theme
-curl -L -o Sweet-Ambar.zip https://github.com/EliverLara/Sweet/releases/download/3.0/Sweet-Ambar.zip
+curl -L -o Sweet-Ambar.zip https://github.com/EliverLara/Sweet/releases/download/v3.0/Sweet-Ambar.zip
 cp Sweet-Ambar.zip ~/.themes/
 xfconf-query -c xfwm4 -p /general/theme -s Sweet-Ambar
 
