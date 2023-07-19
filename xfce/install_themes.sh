26971588ff770f6a7020557ee08065229c595163
Giorgi Sharmiashvili <gsharmiashvili99@gmail.com>
2023-07-20 00:19:59 +0400

    update theme installation

--- xfce/install_themes.sh
+++ xfce/install_themes.sh
@@ -4,13 +4,13 @@
 curl -L -o Sweet-Ambar.zip https://github.com/EliverLara/Sweet/releases/download/v3.0/Sweet-Ambar.zip && unzip Sweet-Ambar
 cp -r ./Sweet-Ambar ~/.themes/
 xfconf-query -c xfwm4 -p /general/theme -s Sweet-Ambar
-rm -r ./Sweet-Amber
+rm -r ./Sweet-Ambar && rm ./Sweet-Ambar.zip
 
 # Install GTK theme
 git clone https://www.opencode.net/ju1464/Bazik.git
 cp -r ./Bazik/GTK-Gnome/Bazik_Dark_Green ~/.themes/
 xfconf-query -c xsettings -p /Net/ThemeName -s "Bazik_Dark_Green"
-rm -r ./Bazik
+rm -rf ./Bazik
 
 # Install Icon theme
 yay -Sg oranchelo-icon-theme-git
